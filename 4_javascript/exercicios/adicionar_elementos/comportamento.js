647de584d73ec36e467fbca67ad042000589e601
Caio Ramos <caio.campsv@gmail.com>
2021-11-30 19:59:30 -0300

    Renomeando arquivos para manter o padrão das demais pastas

--- 4_javascript/exercicios/adicionar_elementos/comportamento.js
+++ 4_javascript/exercicios/adicionar_elementos/comportamento.js
@@ -1,15 +1,31 @@
 function adicionarH1() {
+  var h1 = document.createElement("h1");
+  h1.appendChild(document.createTextNode("Elemento H1 adicionado pelo JS"));
 
+  var areaH1 = document.getElementById("area_h1");
+  areaH1.appendChild(h1);
 }
 
 function adicionarH2() {
+  var h2 = document.createElement("h2");
+  h2.appendChild(document.createTextNode("Elemento H2 adicionado pelo JS"));
 
+  var areaH2 = document.getElementById("area_h2");
+  areaH2.appendChild(h2);
 }
 
 function adicionarP() {
+  var p = document.createElement("p");
+  p.appendChild(document.createTextNode("Elemento P adicionado pelo JS"));
 
+  var areaP = document.getElementById("area_p");
+  areaP.appendChild(p);
 }
 
 function adicionarInput() {
+  var input = document.createElement("input");
+  input.type = "password";
 
+  var areaInput = document.getElementById("area_input");
+  areaInput.appendChild(input);
 }
